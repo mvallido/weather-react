c6704db7755c25d444384459045a732c240d2d48
mvallido <vallido.michael@gmail.com>
2021-07-07 23:13:48 -0700

    Fetching from OpenWeather

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 const api = {
   key: "41506eebbc55599f4ed8744ac62b4c8a",
   base: "http://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setweather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter"){
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setQuery('');
+          setweather(result);
+          console.log(result);
+        });
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+ 
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
     <div className="app">
       <main>
         <div className="search-box">
-          <input type="text" className="search-bar" placeholder="Search..." />
+          <input type="text" className="search-bar" placeholder="Search..." onChange={e => setQuery(e.target.value)} value={query} onKeyPress={search}/>
         </div>
+        <div className="location-box">
+          <div className="location">Vancouver, BC</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">
+            26c
+          </div>
+          <div className="weather">
+            Sunny
+          </div>
+        </div>
+
+
       </main>
     </div>
   );
